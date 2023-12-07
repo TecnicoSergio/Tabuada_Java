9b8794a2f31a7008c77a40edab4265c63bd3321c
Sergio Rodrigues dos Santos <sergiotinfo3@gmail.com>
2023-12-07 20:46:52 -0300

    CommitInicial

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 20; i++) {
+            for (int j = 1; j <= 20; j++) {
+                System.out.println(j + " x " + i + " = " + j * i);
+            }
+        }
+
+
+        for(int i = 1; i <= 10; i++){
+            int soma = 0;
+            soma += i;
+            System.out.println(soma);
+
+        }
+
+
+    }
+}
